e85792e9d3eaf2e0c4262ffb85eee23519f2c95c
amirelrahep <amirelrahep@gmail.com>
2022-02-05 22:32:32 -0500

    Solved issue where content was underneath the navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Typography from "@mui/material/Typography";
+import Toolbar from "@mui/material/Toolbar";
 
 
 const App = () => {
     return (
         <div>
             <NavBar/>
+            // empty toolbar to push content away fom underneath the navbar
+            <Toolbar/>
 
-            <Typography variant="h1">Hello World</Typography>
             <Typography variant="h1">Hello World</Typography>
         </div>
     );
