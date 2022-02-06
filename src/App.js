df72fb7ca38921639f97c32191aa2c3316c75248
amirelrahep <amirelrahep@gmail.com>
2022-02-05 22:05:17 -0500

    Added navigation bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import NavBar from "./components/NavBar";
+import Typography from "@mui/material/Typography";
 
 
 const App = () => {
-  return (
-      <div>
-        <h1>
-          Hello World
-        </h1>
-      </div>
-  );
+    return (
+        <div>
+            <NavBar/>
+
+            <Typography variant="h1">Hello World</Typography>
+            <Typography variant="h1">Hello World</Typography>
+        </div>
+    );
 }
 
 export default App;
