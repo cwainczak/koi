9b84f9dba7d1c603b0296f8bb4d0697c6187f5ca
amirelrahep <amirelrahep@gmail.com>
2022-02-09 15:07:52 -0500

    Added pages

---
+++ src/pages/Chats.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+
+const Chats = () => {
+    return (
+        <div>
+            <Typography variant="h2">Chats Page</Typography>
+        </div>
+    );
+};
+
+export default Chats;
---
+++ src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Typography from "@mui/material/Typography";
+
+
+const Profile = () => {
+    return (
+        <div>
+            <Typography variant="h2">Profile Page</Typography>
+        </div>
+    );
+};
+
+export default Profile;
