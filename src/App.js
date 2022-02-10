9e9a82ffdf41ae1bd5309c680900e34022aedc0f
amirelrahep <amirelrahep@gmail.com>
2022-02-09 20:31:15 -0500

    Removed navigation bar if on the sign-in page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Toolbar from "@mui/material/Toolbar";
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
+import SignIn from "./pages/SignIn";
 import Home from "./pages/Home";
 import Friends from "./pages/Friends";
 import Chats from "./pages/Chats";
@@ -11,16 +12,22 @@ import Profile from "./pages/Profile";
 const App = () => {
     return (
         <div>
-            <NavBar/>
-            {/* empty toolbar to push content away fom underneath the navbar */}
-            <Toolbar/>
-            <br/>
-
             <Switch>
-                <Route exact from="/Home" render={props => <Home {...props} />} />
-                <Route exact from="/Friends" render={props => <Friends {...props} />} />
-                <Route exact path="/chats" render={props => <Chats {...props} />} />
-                <Route exact path="/Profile" render={props => <Profile {...props} />} />
+                <Route exact from="/SignIn" render={props => <SignIn {...props} />}/>
+
+                <div>
+                    <NavBar/>
+                    {/* empty toolbar to push content away fom underneath the navbar */}
+                    <Toolbar/>
+                    <br/>
+
+                    <Switch>
+                        <Route exact from="/Home" render={props => <Home {...props} />}/>
+                        <Route exact from="/Friends" render={props => <Friends {...props} />}/>
+                        <Route exact path="/chats" render={props => <Chats {...props} />}/>
+                        <Route exact path="/Profile" render={props => <Profile {...props} />}/>
+                    </Switch>
+                </div>
             </Switch>
         </div>
     );
