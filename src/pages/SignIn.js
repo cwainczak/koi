bcad6ad0b2a36596e31dfd8169530c9cb0131a98
amirelrahep <amirelrahep@gmail.com>
2022-02-09 20:37:13 -0500

    Changed image

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -35,7 +35,7 @@ const SignIn = (props) => {
                 sm={4}
                 md={7}
                 sx={{
-                    backgroundImage: 'url(https://images.unsplash.com/photo-1585936529565-1871537209e3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3987&q=80)',
+                    backgroundImage: 'url(https://images.hdqwalls.com/wallpapers/koi-fishes-minimal-4k-vh.jpg)',
                     backgroundRepeat: 'no-repeat',
                     backgroundColor: (t) =>
                         t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
