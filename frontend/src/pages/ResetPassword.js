ea3a22db6ecfa086884e443fca92bef9bc5be31f
amirelrahep <amirelrahep@gmail.com>
2022-02-12 17:52:20 -0500

    Added reset password page
    On this page the user will enter the code they were emailed, their new password, and confirm their password. Upon completion, it will redirect them back to the sign-in page where they can sign in with their new password.

---
+++ frontend/src/pages/ResetPassword.js
@@ -0,0 +1,107 @@
+import React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Paper from "@mui/material/Paper";
+
+
+const ResetPassword = (props) => {
+    const {history} = props;
+
+    const handleButtonClick = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // eslint-disable-next-line no-console
+        console.log({
+            code: data.get('code'),
+            password: data.get('password'),
+            confirmPassword: data.get('confirmPassword'),
+        });
+
+        history.push("/SignIn");
+    };
+
+    return (
+        <Grid container component="main" sx={{height: '100vh'}}>
+            <CssBaseline/>
+            <Grid
+                item
+                xs={false}
+                sm={4}
+                md={7}
+                sx={{
+                    backgroundImage: 'url(https://images.hdqwalls.com/wallpapers/koi-fishes-minimal-4k-vh.jpg)',
+                    backgroundRepeat: 'no-repeat',
+                    backgroundColor: (t) =>
+                        t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <Box
+                    sx={{
+                        my: 8,
+                        mx: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{m: 1, bgcolor: '#e4b109'}}>
+                        <LockOutlinedIcon/>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Reset Password
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleButtonClick} sx={{mt: 1}}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="code"
+                            label="Code"
+                            name="code"
+                            autoComplete="code"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="newPassword"
+                            label="New Password"
+                            name="newPassword"
+                            autoComplete="newPassword"
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="confirmPassword"
+                            label="Confirm Password"
+                            type="confirmPassword"
+                            id="confirmPassword"
+                            autoComplete="confirmPassword"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{mt: 3, mb: 2}}
+                        >
+                            Reset Password
+                        </Button>
+                    </Box>
+                </Box>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default ResetPassword;
