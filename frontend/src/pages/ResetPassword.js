cb0dbfd058b47d3e97ea6fa8599de66f0fad9a24
amirelrahep <amirelrahep@gmail.com>
2022-02-12 18:05:24 -0500

    Changed imports wrapped in single quotes to double quotes

--- frontend/src/pages/ResetPassword.js
+++ frontend/src/pages/ResetPassword.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 
 
