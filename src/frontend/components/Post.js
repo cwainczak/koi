b6d022ee2e2c5b9ad292e904e7c82b7f24b7ea30
amirelrahep <amirelrahep@gmail.com>
2022-02-15 23:24:22 -0500

    Added theme

--- src/frontend/components/Post.js
+++ src/frontend/components/Post.js
@@ -7,42 +7,54 @@ import Typography from '@mui/material/Typography';
 import Avatar from "@mui/material/Avatar";
 import Divider from "@mui/material/Divider";
 import Grid from "@mui/material/Grid";
+import {createTheme, ThemeProvider} from "@mui/material";
 
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#e4b109"
+        }
+    }
+});
 
 const Post = (props) => {
     return (
-        <Card sx={{height: '100%', display: 'flex', flexDirection: 'column'}}>
-            <CardContent sx={{flexGrow: 1}}>
-                {/* user icon and name */}
-                <Grid container spacing={3}>
-                    <Grid item xs="auto">
-                        <Avatar alt={props.username} src="/static/images/avatar/2.jpg"/>
+        <ThemeProvider theme={theme}>
+            <Card sx={{height: '100%', display: 'flex', flexDirection: 'column'}}>
+                <CardContent sx={{flexGrow: 1}}>
+                    {/* user icon and name */}
+                    <Grid container spacing={3}>
+                        <Grid item xs="auto">
+                            <Avatar alt={props.username} src="/static/images/avatar/2.jpg"/>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography gutterBottom variant="body1" component="h1">{props.username}</Typography>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                        <Typography gutterBottom variant="body1" component="h1">{props.username}</Typography>
-                    </Grid>
-                </Grid>
 
-                <br/>
-                <Divider/>
-                <br/>
+                    <br/>
+                    <Divider/>
+                    <br/>
 
-                <Typography gutterBottom variant="h6" component="h2">{props.title}</Typography>
-                <Typography>{props.content}</Typography>
+                    <Typography gutterBottom variant="h6" component="h2">{props.title}</Typography>
+                    <Typography>{props.content}</Typography>
 
-                <br/>
-                <br/>
+                    <br/>
+                    <br/>
 
-                <Typography variant="body2" component="h1">Likes {props.likes}</Typography>
+                    <Typography variant="body2" component="h1">Likes {props.likes}</Typography>
 
-                <br/>
-                <Divider/>
-            </CardContent>
-            <CardActions>
-                <Button size="small">Like</Button>
-                <Button size="small">Comment</Button>
-            </CardActions>
-        </Card>
+                    <br/>
+                    <Divider/>
+                </CardContent>
+                <CardActions>
+                    <Button size="small">Like</Button>
+                    <Button size="small">Comment</Button>
+                </CardActions>
+            </Card>
+        </ThemeProvider>
     );
 }
 
