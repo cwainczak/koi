93446cfec278c57187de1522870857e45de69786
amirelrahep <amirelrahep@gmail.com>
2022-02-16 20:38:13 -0500

    Reformatted code

--- src/frontend/pages/Home.js
+++ src/frontend/pages/Home.js
@@ -7,31 +7,36 @@ const Home = () => {
     return (
         <Container>
             <Post
-                username="operatingoracle" title="On it differed"
+                username="operatingoracle"
+                title="On it differed"
                 content="On it differed repeated wandered required in. Then girl neat why yet knew rose spot. Moreover property we he kindness greatest be oh striking laughter. In me he at collecting affronting principles apartments. Has visitor law attacks pretend you calling own excited painted. Contented attending smallness it oh ye unwilling. Turned favour man two but lovers. Suffer should if waited common person little oh. Improved civility graceful sex few smallest screened settling. Likely active her warmly has."
                 likes={18}
             />
             <br/>
             <Post
-                username="treatycisco" title="Now residence dashwoods"
+                username="treatycisco"
+                title="Now residence dashwoods"
                 content="Now residence dashwoods she excellent you. Shade being under his bed her. Much read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant horrible but confined day end marriage. Eagerness furniture set preserved far recommend. Did even but nor are most gave hope. Secure active living depend son repair day ladies now."
                 likes={48}
             />
             <br/>
             <Post
-                username="volleyballford" title="Considered discovered ye sentiments"
+                username="volleyballford"
+                title="Considered discovered ye sentiments"
                 content="Considered discovered ye sentiments projecting entreaties of melancholy is. In expression an solicitude principles in do. Hard do me sigh with west same lady. Their saved linen downs tears son add music. Expression alteration entreaties mrs can terminated estimating. Her too add narrow having wished. To things so denied admire. Am wound worth water he linen at vexed."
                 likes={22}
             />
             <br/>
             <Post
-                username="bawlintel" title="Letter wooded direct"
+                username="bawlintel"
+                title="Letter wooded direct"
                 content="Letter wooded direct two men indeed income sister. Impression up admiration he by partiality is. Instantly immediate his saw one day perceived. Old blushes respect but offices hearted minutes effects. Written parties winding oh as in without on started. Residence gentleman yet preserved few convinced. Coming regret simple longer little am sister on. Do danger in to adieus ladies houses oh eldest. Gone pure late gay ham. They sigh were not find are rent."
                 likes={12}
             />
             <br/>
             <Post
-                username="modifiedmicrosoft" title="Insipidity the sufficient discretion"
+                username="modifiedmicrosoft"
+                title="Insipidity the sufficient discretion"
                 content="Insipidity the sufficient discretion imprudence resolution sir him decisively. Proceed how any engaged visitor. Explained propriety off out perpetual his you. Feel sold off felt nay rose met you. We so entreaties cultivated astonished is. Was sister for few longer mrs sudden talent become. Done may bore quit evil old mile. If likely am of beauty tastes."
                 likes={21}
             />
