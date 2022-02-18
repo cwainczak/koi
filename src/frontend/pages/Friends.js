21c298bc01a7694883ae984969c43249fea302b3
amirelrahep <amirelrahep@gmail.com>
2022-02-18 17:57:05 -0500

    Dynamically created friend cards from array

--- src/frontend/pages/Friends.js
+++ src/frontend/pages/Friends.js
@@ -2,31 +2,28 @@ import React from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Friend from "../components/Friend";
+import FriendObj from "../../backend/FriendObj";
+
+
+let friends = [new FriendObj("dellmultiple", 15), new FriendObj("ibmdifference", 20),
+    new FriendObj("volkswagonbream", 25), new FriendObj("nikemelt", 30),
+    new FriendObj("ebayclassic", 35), new FriendObj("googlewillow", 40)];
 
 
 const Friends = () => {
     return (
         <Container>
+
             <Grid container rowSpacing={2} columnSpacing={2}>
-                <Grid item xs={12} sm={6}>
-                    <Friend username="dellmultiple" friends="15"/>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Friend username="ibmdifference" friends="20"/>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Friend username="volkswagonbream" friends="25"/>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Friend username="nikemelt" friends="30"/>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Friend username="ebayclassic" friends="35"/>
-                </Grid>
-                <Grid item xs={12} sm={6}>
-                    <Friend username="googlewillow" friends="40"/>
-                </Grid>
+                {friends.map((friend) => (
+                    <>
+                        <Grid item xs={12} sm={6}>
+                            <Friend username={friend.username} friends={friend.mutualFriends}/>
+                        </Grid>
+                    </>
+                ))}
             </Grid>
+
         </Container>
     );
 };
