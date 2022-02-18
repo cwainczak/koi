bf2af27deeaa1305d093c4020c07d42dce90789f
amirelrahep <amirelrahep@gmail.com>
2022-02-18 17:39:08 -0500

    Added sample friends

--- src/frontend/pages/Friends.js
+++ src/frontend/pages/Friends.js
@@ -1,12 +1,33 @@
 import React from "react";
-import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Friend from "../components/Friend";
 
 
 const Friends = () => {
     return (
-        <div>
-            <Typography variant="h2">Friends Page</Typography>
-        </div>
+        <Container>
+            <Grid container rowSpacing={2} columnSpacing={2}>
+                <Grid item xs={12} sm={6}>
+                    <Friend username="dellmultiple" friends="15"/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Friend username="ibmdifference" friends="20"/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Friend username="volkswagonbream" friends="25"/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Friend username="nikemelt" friends="30"/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Friend username="ebayclassic" friends="35"/>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                    <Friend username="googlewillow" friends="40"/>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
