6eced770e8f89515c10268909df19f4e97be8b14
amirelrahep <amirelrahep@gmail.com>
2022-02-19 19:46:37 -0500

    Added navigation between different sections on friends page
    The different sections include:
     - friends (the user's current friends)
     - friend requests (friend requests that the user can accept or deny)
     - find friends (other people that the user can send a friend request too)

--- src/frontend/pages/Friends.js
+++ src/frontend/pages/Friends.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
+import SwipeableViews from "react-swipeable-views";
 import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import AppBar from "@mui/material/AppBar";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 import Friend from "../components/Friend";
 import FriendObj from "../../backend/FriendObj";
 
@@ -10,20 +16,64 @@ let friends = [new FriendObj("dellmultiple", 15), new FriendObj("ibmdifference",
     new FriendObj("ebayclassic", 35), new FriendObj("googlewillow", 40)];
 
 
+function TabPanel(props) {
+    const {children, value, index} = props;
+
+    return (
+        <Typography>
+            {value === index && <Box sx={{p: 3}}>{children}</Box>}
+        </Typography>
+    );
+}
+
 const Friends = () => {
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    const handleChangeIndex = (index) => {
+        setValue(index);
+    };
+
     return (
         <Container>
-
-            <Grid container rowSpacing={2} columnSpacing={2}>
-                {friends.map((friend) => (
-                    <>
-                        <Grid item xs={12} sm={6}>
-                            <Friend username={friend.username} friends={friend.mutualFriends}/>
-                        </Grid>
-                    </>
-                ))}
-            </Grid>
-
+            <AppBar position="static">
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    variant="fullWidth"
+                >
+                    <Tab label="Friends"/>
+                    <Tab label="Friend Requests"/>
+                    <Tab label="Find Friends"/>
+                </Tabs>
+            </AppBar>
+            <SwipeableViews
+                index={value}
+                onChangeIndex={handleChangeIndex}
+            >
+                <TabPanel>
+                    <Grid container rowSpacing={2} columnSpacing={2}>
+                        {friends.map((friend) => (
+                            <>
+                                <Grid item xs={12} sm={6}>
+                                    <Friend username={friend.username} friends={friend.mutualFriends}/>
+                                </Grid>
+                            </>
+                        ))}
+                    </Grid>
+                </TabPanel>
+                <TabPanel>
+                    Friend Requests
+                </TabPanel>
+                <TabPanel>
+                    Find Friends
+                </TabPanel>
+            </SwipeableViews>
         </Container>
     );
 };
