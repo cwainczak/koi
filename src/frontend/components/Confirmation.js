663ebc3c002b21737b59a48c1f887208015f6e94
amirelrahep <amirelrahep@gmail.com>
2022-02-28 18:51:56 -0500

    Update the Confirmation.js component to be more reusable

--- src/frontend/components/Confirmation.js
+++ src/frontend/components/Confirmation.js
@@ -1,6 +1,4 @@
 import React from "react";
-import IconButton from "@mui/material/IconButton";
-import PersonRemoveIcon from "@mui/icons-material/PersonRemove";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
@@ -10,38 +8,17 @@ import Button from "@mui/material/Button";
 
 
 const Confirmation = (props) => {
-    const [open, setOpen] = React.useState(false);
-
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-
-    const handleClose = () => {
-        setOpen(false);
-    };
-
-    const handleRemove = () => {
-        setOpen(false);
-        // todo - remove friend from user - this might have to come from the parent class
-    };
-
     return (
-        <div>
-            <IconButton>
-                <PersonRemoveIcon style={{color: "#b1b3b9"}} onClick={handleClickOpen}/>
-            </IconButton>
-
-            <Dialog open={open} onClose={handleClose}>
-                <DialogTitle>{props.title}</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>{props.text}</DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Cancel</Button>
-                    <Button onClick={handleRemove}>Remove</Button>
-                </DialogActions>
-            </Dialog>
-        </div>
+        <Dialog open={props.isOpen} onClose={props.handleClose}>
+            <DialogTitle>{props.title}</DialogTitle>
+            <DialogContent>
+                <DialogContentText>{props.message}</DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={props.handleClose}>{props.button1}</Button>
+                <Button onClick={props.handleAction}>{props.button2}</Button>
+            </DialogActions>
+        </Dialog>
     );
 }
 
