81242384e0a5ab724efcf850ecad3ad2544b4919
amirelrahep <amirelrahep@gmail.com>
2022-02-28 19:11:06 -0500

    Changed the action on the card to open a popup menu

--- src/frontend/components/Friend.js
+++ src/frontend/components/Friend.js
@@ -3,6 +3,10 @@ import {createTheme, ThemeProvider} from "@mui/material";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 import Confirmation from "../components/Confirmation";
 
 
@@ -25,6 +29,31 @@ const theme = createTheme({
 });
 
 const Friend = (props) => {
+    const [anchorElOptions, setAnchorElOptions] = React.useState(null);
+    const [isOpen, setIsOpen] = React.useState(false);
+
+    const handleOpenOptionsMenu = (event) => {
+        setAnchorElOptions(event.currentTarget);
+    };
+
+    const handleCloseOptionsMenu = () => {
+        setAnchorElOptions(null);
+    };
+
+    const handelOpenDialog = () => {
+        setAnchorElOptions(null);
+        setIsOpen(true);
+    }
+
+    const handelCloseDialog = () => {
+        setIsOpen(false);
+    }
+
+    const handelDialogAction = () => {
+        setIsOpen(false);
+        // todo - remove friend from user
+    }
+
     return (
         <ThemeProvider theme={theme}>
             <Card>
@@ -33,15 +62,33 @@ const Friend = (props) => {
                         <Avatar {...shrinkUsername(props.username)}/>
                     }
                     action={
-                        <Confirmation
-                            title="Remove Friend?"
-                            text={"Are you sure you want to remove " + props.username + " from being your friend?"}
-                        />
+                        <IconButton>
+                            <MoreVertIcon onClick={handleOpenOptionsMenu}/>
+                        </IconButton>
                     }
                     title={props.username}
                     subheader={props.friends + " mutual friends"}
                 />
+                <Menu
+                    id="options-menu"
+                    anchorEl={anchorElOptions}
+                    open={Boolean(anchorElOptions)}
+                    onClose={handleCloseOptionsMenu}
+                >
+                    <MenuItem onClick={handleCloseOptionsMenu}>View Profile</MenuItem>
+                    <MenuItem onClick={handelOpenDialog}>Remove Friend</MenuItem>
+                </Menu>
             </Card>
+
+            <Confirmation
+                isOpen={isOpen}
+                handleClose={handelCloseDialog}
+                handleAction={handelDialogAction}
+                title="Remove Friend?"
+                message={"Are you sure you want to remove " + props.username + " from being your friend?"}
+                button1={"Cancel"}
+                button2={"Remove"}
+            />
         </ThemeProvider>
     );
 }
