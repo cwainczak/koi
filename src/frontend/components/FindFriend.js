04bc4c4f076b1a624d2a6f00679372bc906f6ad1
amirelrahep <amirelrahep@gmail.com>
2022-03-02 09:53:25 -0500

    Added card to display new friends for user to add

---
+++ src/frontend/components/FindFriend.js
@@ -0,0 +1,52 @@
+import React from "react";
+import {createTheme, ThemeProvider} from "@mui/material";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import AddIcon from '@mui/icons-material/Add';
+
+
+function shrinkUsername(name) {
+    return {
+        children: `${name.split(' ')[0][0]}`
+    };
+}
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#e4b109"
+        },
+        secondary: {
+            main: "#b1b3b9"
+        }
+    }
+});
+
+const CurrentFriend = (props) => {
+    const handleAddFriend = () => {
+        // todo - add friend to user
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Card>
+                <CardHeader
+                    avatar={
+                        <Avatar {...shrinkUsername(props.username)}/>
+                    }
+                    action={
+                        <IconButton>
+                            <AddIcon onClick={handleAddFriend}/>
+                        </IconButton>
+                    }
+                    title={props.username}
+                />
+            </Card>
+        </ThemeProvider>
+    );
+}
+
+export default CurrentFriend;
