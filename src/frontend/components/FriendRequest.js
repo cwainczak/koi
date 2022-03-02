b56ffd608b70b7a69245eb7021e16d7e4a1b5554
amirelrahep <amirelrahep@gmail.com>
2022-03-02 09:24:09 -0500

    Added card to display a users' friend requests

---
+++ src/frontend/components/FriendRequest.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {createTheme, ThemeProvider} from "@mui/material";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+
+
+function shrinkUsername(name) {
+    return {
+        children: `${name.split(' ')[0][0]}`
+    };
+}
+
+const theme = createTheme({
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#e4b109"
+        },
+        secondary: {
+            main: "#b1b3b9"
+        }
+    }
+});
+
+const FriendRequest = (props) => {
+    const [anchorElOptions, setAnchorElOptions] = React.useState(null);
+
+    const handleOpenOptionsMenu = (event) => {
+        setAnchorElOptions(event.currentTarget);
+    };
+
+    const handleCloseOptionsMenu = () => {
+        setAnchorElOptions(null);
+    };
+
+    const handleAccept = () => {
+        setAnchorElOptions(null);
+        // todo - add friend to user and remove from list of requests
+    };
+
+    const handleDeny = () => {
+        setAnchorElOptions(null);
+        // todo - remove friend from lists of requests
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Card>
+                <CardHeader
+                    avatar={
+                        <Avatar {...shrinkUsername(props.username)}/>
+                    }
+                    action={
+                        <IconButton>
+                            <MoreVertIcon onClick={handleOpenOptionsMenu}/>
+                        </IconButton>
+                    }
+                    title={props.username}
+                    subheader={props.friends + " mutual friends"}
+                />
+                <Menu
+                    id="options-menu"
+                    anchorEl={anchorElOptions}
+                    open={Boolean(anchorElOptions)}
+                    onClose={handleCloseOptionsMenu}
+                >
+                    <MenuItem onClick={handleAccept}>Accept</MenuItem>
+                    <MenuItem onClick={handleDeny}>Deny</MenuItem>
+                </Menu>
+            </Card>
+        </ThemeProvider>
+    );
+}
+
+export default FriendRequest;
