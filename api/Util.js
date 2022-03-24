e7a8c0e3a63df7ccb1ff45b32911abc27c5fab3e
Cooper Wainczak <gainczak@gmail.com>
2022-03-24 12:07:18 -0400

    Comments about how BCrypt works internally

--- api/Util.js
+++ api/Util.js
@@ -1,3 +1,9 @@
+/*
+When hashing with BCrypt, a new salt is generated, used, and stored within the hash.
+When comparing a password to its hash, it uses the stored salt in the hash to hash the plain text password.
+This will yield the same hash if the plain text password is correct.
+TLDR: same salts, same plain text password, same hash
+ */
 
 const bcrypt = require("bcrypt")
 // the higher the rounds, the more secure, but the longer it takes to generate
