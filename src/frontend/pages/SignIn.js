1806b508d0acdb937e8d49b010f01b9814cce4af
amirelrahep <amirelrahep@gmail.com>
2022-04-06 18:22:30 -0400

    Reformatted code

--- src/frontend/pages/SignIn.js
+++ src/frontend/pages/SignIn.js
@@ -10,8 +10,8 @@ import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { login } from "../../backend/User"
-import { removeWhiteSpace } from "../../backend/Util";
+import {login} from "../../backend/User"
+import {removeWhiteSpace} from "../../backend/Util";
 
 
 const SignIn = (props) => {
@@ -26,7 +26,7 @@ const SignIn = (props) => {
 
     try {
         sucPassReset = history.location.state.passReset
-    } catch(e) {
+    } catch (e) {
         console.log(e)
     }
 
@@ -37,26 +37,24 @@ const SignIn = (props) => {
         let entUser = removeWhiteSpace(data.get("username"))
         let entPass = removeWhiteSpace(data.get("password"))
         let errDialog = document.getElementById("invalidCredentialsLogin")
-        if (entUser === "" || entPass === ""){
+
+        if (entUser === "" || entPass === "") {
             errDialog.hidden = false
             errDialog.textContent = "Please fill in all fields"
-        }
-        else {
+        } else {
             // login returns -1 if user doesn't exist, and UserID if user does exist
             const curUserID = await login(entUser, entPass)
             console.log(curUserID)
-            if (curUserID !== -1){
+            if (curUserID !== -1) {
                 // disable button
                 handleLoading();
 
                 history.push("/Home")
-            }
-            else {
+            } else {
                 errDialog.hidden = false
                 errDialog.textContent = "Invalid Username or Password"
             }
         }
-
     };
 
     return (
@@ -93,7 +91,8 @@ const SignIn = (props) => {
                         Sign In
                     </Typography>
                     <Container component="form" noValidate onSubmit={handleButtonClick} sx={{mt: 1}} maxWidth="sm">
-                        <Typography id={"passResetMessage"} fontSize={12} color={"green"} paddingTop={1.5} textAlign={"center"} hidden={!sucPassReset}>
+                        <Typography id={"passResetMessage"} fontSize={12} color={"green"} paddingTop={1.5}
+                                    textAlign={"center"} hidden={!sucPassReset}>
                             Your password has been reset!
                         </Typography>
                         <TextField
@@ -114,7 +113,8 @@ const SignIn = (props) => {
                             type="password"
                             id="password"
                         />
-                        <Typography id={"invalidCredentialsLogin"} fontSize={12} color={"red"} paddingTop={1.5} textAlign={"center"} hidden={true}>
+                        <Typography id={"invalidCredentialsLogin"} fontSize={12} color={"red"} paddingTop={1.5}
+                                    textAlign={"center"} hidden={true}>
 
                         </Typography>
                         <LoadingButton
