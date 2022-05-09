fd51da757c0c2482a065b5fe604dfdedb1c98ca8
amirelrahep <amirelrahep@gmail.com>
2022-05-09 12:54:46 -0400

    Changed alert message for post creation

--- src/frontend/pages/Home.js
+++ src/frontend/pages/Home.js
@@ -7,20 +7,33 @@ import PostDialog from "../components/PostDialog";
 import {removeWhiteSpace} from "../../backend/Util";
 import {createUserPost} from "../../backend/UserPost";
 import {curUser} from "../../backend/UserObj";
-import Typography from "@mui/material/Typography";
+import {Snackbar, Alert} from "@mui/material";
 
 
-let posts = [new PostObj(1,"username 1", "title 1", "content 1", 1, [["username 1", "comment 1"], ["username 2", "comment 2"]]),
-    new PostObj(2,"username 2", "title 2", "content 2", 1, [["username 1", "comment 1"], ["username 2", "comment 2"]])]
+let posts = [new PostObj(1, "username 1", "title 1", "content 1", 1, [["username 1", "comment 1"], ["username 2", "comment 2"]]),
+    new PostObj(2, "username 2", "title 2", "content 2", 1, [["username 1", "comment 1"], ["username 2", "comment 2"]])]
 
 
 const Home = () => {
-    const [successfulPostCreationHidden, setSuccessfulPostCreationHidden] = React.useState(true)
+    // popup snackbar alert message
+    const [isSnackbarOpen, setIsSnackbarOpen] = React.useState(false);
+    const [alertSeverity, setAlertSeverity] = React.useState("");
+    const [alertMessage, setAlertMessage] = React.useState("");
 
-    useEffect(() => {
-        setSuccessfulPostCreationHidden(true)
-    }, [])
+    const handleOpenSnackbar = (severity, message) => {
+        setAlertSeverity(severity);
+        setAlertMessage(message);
+        setIsSnackbarOpen(true);
+    };
 
+    const handleCloseSnackbar = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setIsSnackbarOpen(false);
+    };
+
+    // post dialog to create post
     const [isPostDialogOpen, setIsPostDialogOpen] = React.useState(false);
 
     const handleOpenPostDialog = () => {
@@ -50,12 +63,13 @@ const Home = () => {
             errDialog.textContent = "Content is too long. (max: 1000 characters)";
             errDialog.hidden = false;
         } else {
+            setIsPostDialogOpen(false);
             let isSuccess = await createUserPost(curUser.UserID, entTitle, entContent);
 
             if (isSuccess) {
-                setIsPostDialogOpen(false);
-                setSuccessfulPostCreationHidden(false)
+                handleOpenSnackbar("success", "Your post has been created!");
             } else {
+                handleOpenSnackbar("warning", "Something went wrong! Your post was not created.");
                 console.log("Something went wrong!");
             }
         }
@@ -63,16 +77,6 @@ const Home = () => {
 
     return (
         <Container>
-            <Typography
-                id={"newPostMsg"}
-                fontSize={12}
-                color={"darkorange"}
-                textAlign={"center"}
-                hidden={successfulPostCreationHidden}
-            >
-                Your post has been created!
-            </Typography>
-
             <Button
                 fullWidth
                 variant="contained"
@@ -84,7 +88,7 @@ const Home = () => {
             <br/>
             <br/>
 
-            {posts.map((post,index) => (
+            {posts.map((post, index) => (
                 <>
                     <Post
                         key={index}
@@ -107,6 +111,22 @@ const Home = () => {
                 handleClose={handleClosePostDialog}
                 handleAction={handlePostDialogAction}
             />
+
+            <Snackbar
+                open={isSnackbarOpen}
+                autoHideDuration={5000}
+                onClose={handleCloseSnackbar}
+                anchorOrigin={{vertical: 'top', horizontal: "center"}}
+            >
+                <Alert
+                    onClose={handleCloseSnackbar}
+                    variant="filled"
+                    severity={alertSeverity}
+                    sx={{width: '100%'}}
+                >
+                    {alertMessage}
+                </Alert>
+            </Snackbar>
         </Container>
     );
 };
